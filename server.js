87244b3b738919a776fcec5e7fea642bef42e791
Jason Boudreaux <js.bdrx@gmail.com>
2022-03-05 00:20:32 -0600

    deploying

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/budget-tracker', {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
